409700ae7137f92fb864026da71dacafbc79547d
DESKTOP-0FUK34J\DELL <kaifa.infoseek@gmail.com>
2021-09-21 19:28:01 +0530

    Learned about setting headers ,getting urls and giving response

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.headers);
+    res.setHeader('Content-Type', 'text/html');
 
-    // Hards Exits the Event Loop
-    // process.exit();
+    res.write(`
+    <h1> Hello Kaif</h1>
+    `);
+    res.end();
 })
 
-server.listen(4000); 
+server.listen(4000);  
