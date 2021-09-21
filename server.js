c8da3e83a3ac259936d909aeda6d157ea59520c3
DESKTOP-0FUK34J\DELL <kaifa.infoseek@gmail.com>
2021-09-21 20:30:48 +0530

    Used file write and save submitted data in that file

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.headers);
-    res.setHeader('Content-Type', 'text/html');
+const fs = require('fs');
 
-    res.write(`
-    <h1> Hello Kaif</h1>
-    `);
+
+const server = http.createServer((req,res)=>{
+
+    if(req.url == '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+        <form action="/credentials" method = "post">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+        </form> 
+        `);
+    }
+    else if(req.url == '/credentials' && req.method == "POST") {
+        
+        const body = [];
+        req.on('data', (chunk)=> {
+            body.push(chunk);
+        });
+        req.on('end', ()=> {
+            const parsedBody = Buffer.concat(body).toString();
+            fs.writeFileSync('Credentials.txt', parsedBody);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+    }
+    else {
+        console.log(req.url);
+        res.write(`
+        <h1> Hello Kaif</h1>
+        `);
+    }
     res.end();
 })
 
